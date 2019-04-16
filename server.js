b0bf787a4535ee771612ed00bbc6a569fd78ded4
ironsquishy <spacecore88@gmail.com>
2019-04-16 09:45:12 -0700

    Temporary remove server network identificaiton.

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 /* Start of Server*/
-var os = require('os');
-
+//var os = require('os');
 /*Print out server network interfaces*/
-var en0 = os.networkInterfaces().en0;
-en0.forEach(item => {
-    console.log(`Network en0: ${item.family} < ${item.address} >`);
-})
+// var en0 = os.networkInterfaces().en0;
+// en0.forEach(item => {
+//     console.log(`Network en0: ${item.family} < ${item.address} >`);
+// })
 
 
 require('./src');
